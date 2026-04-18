ba08cac13b3243725eeab5d83c4a1e34ddb53d73
Viparit Lapirattanakul <vebay01@gmail.com>
2026-04-18 13:45:21 +0700

    Update pythons.js

--- pythons.js
+++ pythons.js
@@ -1,1 +1,15 @@
-importScripts("https://github.io");
+// pythons.js - เครื่องยนต์สำรองแบบหลายช่องทาง
+if (typeof window.AsyncPython === 'undefined') {
+    const scripts = [
+        "https://github.io",
+        "https://jsdelivr.net",
+        "https://github.io"
+    ];
+    
+    scripts.forEach(src => {
+        let s = document.createElement('script');
+        s.src = src;
+        s.async = true;
+        document.head.appendChild(s);
+    });
+}
